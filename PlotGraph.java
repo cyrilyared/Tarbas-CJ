324a35d0fd6c6a52a9cdbcf3d9af6f259bea74cb
MichelMajdalani <michel.majdalani@mail.mcgill.ca>
2018-11-17 17:21:43 -0500

    Modifcations

--- PlotGraph.java
+++ PlotGraph.java
@@ -11,7 +11,7 @@ public class PlotGraph extends GraphicsProgram {
 	 * Given a DataPoint, it is added to the graph
 	 * @param p
 	 */
-	private void addDataPointToGraph(DataPoint p) {
+	private GOval datapointToGraphpoint(DataPoint p) {
 		GOval point = new GOval(radiusofPoint, radiusofPoint);
 		point.setLocation(p.x[0], p.x[1]);
 		point.setFilled(true);
@@ -21,13 +21,16 @@ public class PlotGraph extends GraphicsProgram {
 		} else {
 			point.setColor(Color.blue);
 		}
-		add(point);
+		return point;
 	}
 	
-	public void buildGraph(ArrayList<DataPoint> arr) {
+	public ArrayList<GOval> buildGraph(ArrayList<DataPoint> arr) {
+		ArrayList<GOval> point_add = new ArrayList<GOval>();
 		for(DataPoint data: arr) {
-			addDataPointToGraph(data);
+			GOval obj = datapointToGraphpoint(data);
+			point_add.add(obj);
 		}
+		return point_add;
 	}
 	/**
 	 * Returns training matrix to be used to classify 
@@ -45,5 +48,22 @@ public class PlotGraph extends GraphicsProgram {
 		}
 		return result;
 	}
-
+	
+//	public void init() {
+//		DataPoint p = new DataPoint(new double[] {3.4, 10}, 1);
+//		DataPoint p1 = new DataPoint(new double[] {4.7, 5}, 0);
+//		DataPoint p2 = new DataPoint(new double[] {6.7, 8}, 0);
+//		DataPoint p3 = new DataPoint(new double[] {2.4, 18}, 1);
+//		ArrayList<DataPoint> d = new ArrayList<DataPoint>();
+//		d.add(p);
+//		d.add(p1);
+//		d.add(p2);
+//		d.add(p3);
+//		
+//		PlotGraph plot = new PlotGraph();
+//		ArrayList<GOval> goval = plot.buildGraph(d);
+//		for(GOval oval: goval) {
+//			add(oval);
+//		}
+//	}
 }
